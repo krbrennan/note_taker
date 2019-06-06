863a975dc18e1d852e332d750d9c4940636e412e
krbrennan <krb.brennan@gmail.com>
2019-06-06 15:17:30 -0400

    items cross out when click checkbox

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@
 // I cant modify the complete status of the items from the child component
 // the TodoItems component needs to have control over the state of the items
 
+// 6.3.2019
+//  Instead of handling control over to todoItems,
+//      make todoItems a class and handle its 'completed' state there,
+//      just like the AddItem component
+
 import React from 'react'
 import ToDoItems from './components/ToDoItems'
 import AddItem from './components/AddItem'
@@ -23,12 +28,26 @@ class App extends React.Component {
     }
     this.handleNewItem = this.handleNewItem.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e){
+    // this is getting called from ToDoItems child component
+    // When the item is clicked (making it complete or not)
+    // I want the items' complteted state to be changed here.
+    let id = e
+      this.state.toDoItems.map(item => {
+        if(item.id === e){
+          item.isComplete = !item.isComplete
+        }
+      })
+
+console.log(this.state.toDoItems)
   }
 
   handleNewItem(event){
-    console.log(event)
 
-    let nextId = this.state.toDoItems.slice(-1).id + 1
+    let nextId = this.state.toDoItems.length+1
     const newState = this.state.toDoItems.concat({id: nextId, text: event, isComplete: false})
 
     this.setState(state => {
@@ -44,7 +63,7 @@ class App extends React.Component {
   }
 
   render(){
-    let toDos = this.state.toDoItems.map(item => <ToDoItems content={item} onChange={this.handleChange}/>)
+    let toDos = this.state.toDoItems.map(item => <ToDoItems content={item} onClick={this.handleClick} onChange={this.handleChange}/>)
     return (
       <div className="App">
         <Header />
--- src/components/ToDoItems.js
+++ src/components/ToDoItems.js
@@ -1,35 +1,60 @@
 import React from 'react'
 
-function ToDoItems(props){
-  let completed = props.content.isComplete === true ? 1 : 0;
-  console.log(props)
-  if(props.content.isComplete === true){
+// function ToDoItems(props){
+//   let completed = props.content.isComplete === true ? 1 : 0;
+//
+//   if(props.content.isComplete === true){
+//     return(
+//       <div className='todo-item'>
+//         <input type='checkbox' defaultChecked={true} /> :
+//         <p style={{textDecoration:'line-through'}}>{props.content.text}</p>
+//       </div>
+//     )
+//   } else {
+//     return (
+//       <div className='todo-item'>
+//         <input type='checkbox' defaultChecked={false} />
+//         <p className='todo-text'>{props.content.text}</p>
+//       </div>
+//     )
+//   }
+// }
+
+class ToDoItems extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      isComplete: false
+    }
+    this.handleCheckbox = this.handleCheckbox.bind(this)
+  }
+
+  handleCheckbox(e){
+    // working. successfully calls back to parent
+    // console.log(this.props.content.id)
+    this.setState({
+      isComplete: !!this.state.isComplete
+    })
+    this.props.onClick(this.props.content.id)
+  }
+
+  render(){
+    let isComplete = this.props.content.isComplete === true ? 1 : 0
     return(
       <div className='todo-item'>
-        <input type='checkbox' defaultChecked={true} /> :
-        <p style={{textDecoration:'line-through'}}>{props.content.text}</p>
-      </div>
-    )
-  } else {
-    return (
-      <div className='todo-item'>
-        <input type='checkbox' defaultChecked={false} />
-        <p className='todo-text'>{props.content.text}</p>
+        <input type='checkbox' onClick={this.handleCheckbox} defaultChecked={isComplete} />
+        <p style={
+          isComplete === 1 ? {textDecoration:'line-through'} : {textDecoration:'none'}
+        }>
+          {this.props.content.text}
+        </p>
       </div>
     )
   }
-  // return(
-  //   <div className='todo-item'>
-  //     {completed ?
-  //       <input type='checkbox' defaultChecked={true} /> :
-  //       <input type='checkbox' defaultChecked={false} />
-  //     }
-  //     {completed === 1 ?
-  //       <p style={{textDecoration:'line-through'}}>{props.content.text}</p> :
-  //       <p className='todo-text'>{props.content.text}</p>
-  //     }
-  //   </div>
-  // )
+
+
+
 }
 
 export default ToDoItems
